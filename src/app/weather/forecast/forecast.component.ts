4ae0acb8f5cd75943a1e7959ef5e985dd1a09979
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-19 19:56:09 -0700

    bootstrap loaded
    bootstrap loaded

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ForecastService } from '../forecast.service';
 
 
@@ -8,11 +9,11 @@ import { ForecastService } from '../forecast.service';
   styleUrls: ['./forecast.component.css']
 })
 export class ForecastComponent implements OnInit {
-  forecastData = [];
+
+  forecast$: Observable <{dateString: string;temp: number;}[]>;
+
   constructor(forecastService: ForecastService) {
-    forecastService.getForecast().subscribe(forecastData => {
-      this.forecastData = forecastData;
-    })
+    this.forecast$ = forecastService.getForecast()
   }
 
   ngOnInit() {}
