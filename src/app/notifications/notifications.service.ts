32594095232bb457603dad649ee6b434f24a87dd
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-20 01:17:51 -0700

    subject / building command structure

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  // ? optional
+  text?: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  messages: Subject<any>;
+
+  constructor() {
+    this.messages = new Subject<Command>();
+  }
+
+  addSuccess(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success'
+    });
+  }
+
+  addError(message: string) {
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+  });
+  }
+
+  clearMessage(id: number) {
+    {
+      this.messages.next({
+        id,
+        type: 'clear'
+    });
+  }
+
+  //only in this component
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
