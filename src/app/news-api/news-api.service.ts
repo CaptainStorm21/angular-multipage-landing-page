17a67f61bf9393525efadb2ab3f75678e138e503
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-20 18:01:15 -0700

    491 - wrapping the input subject

--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -3,10 +3,11 @@ import { Observable, Subject } from 'rxjs';
 import {
   tap,
   map,
-  switchMap
+  switchMap,
+  pluck
 } from 'rxjs/operators';
-
 import { HttpParams, HttpClient } from '@angular/common/http';
+import { AppPage } from '../../../e2e/src/app.po';
 
 interface NewsApiResponse {
   totalResults: number;
@@ -15,9 +16,6 @@ interface NewsApiResponse {
     url: string;
   }[];
 }
-
-
-
 
 @Injectable({
   providedIn: 'root'
@@ -29,13 +27,14 @@ export class NewsApiService {
   private apiKey = '9fb2279398d447b4b4e0e5b0f9282daa';
   private country = 'fr';
 
-  pagesInput: Subject<number>;
-  pagesOutput: Observable<any>;
-  numberofPages: Observable<number>;
+  private pagesInput: Subject<number>;
+          pagesOutput: Observable<any>;
+          numberofPages: Subject<number>;
 
   constructor(
     private http: HttpClient
   ) {
+
     this.pagesInput = new Subject();
     this.pagesOutput = this.pagesInput.pipe(
       map((page) => {
@@ -45,17 +44,24 @@ export class NewsApiService {
           .set('pageSize', String(this.pageSize))
           .set('page', String(page))
       }),
-      switchMap((params) => {
+      switchMap(params => {
         // <NewsApiResponse > tells TS specifically about the structure
         // of the data in the response that we get back
         // tnad ts further knowns that is going on with this switch map operator in general
         // the final mouse over will tell me about th etype of data that is flowing hrough this thing
 
-        return this.http.get <NewsApiResponse >(
-          this.url,
-          {params}
-        )
-      })
-    )
+        return this.http.get<NewsApiResponse>(this.url, { params });
+      }),
+      tap(response => {
+        const totalPages = Math.ceil(response.totalResults / this.pageSize);
+        this.numberofPages.next(totalPages);
+      }),
+      pluck('article')
+    );
   }
+
+  getPages(page: number) {
+    this.pagesInput.next(page)
+  }
+
 }
