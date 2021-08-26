2305f5804955590a0ea01dbbc1e5fad44313a305
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-25 21:11:16 -0700

    504 - pagainator set up

--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class PaginatorComponent implements OnInit {
 
   numberOfPages = 5;
-  
-  constructor() { }
+  currentPage = 1;
+
+  pageOptions: number[];
+
+
+
+  constructor() {
+    this.pageOptions = [
+      this.currentPage - 2,
+      this.currentPage - 1,
+      this.currentPage,
+      this.currentPage + 1,
+      this.currentPage + 2
+    ].filter(pageNumber => pageNumber >= 1 && pageNumber <= this.numberOfPages);
+  }
 
   ngOnInit(): void {
   }
