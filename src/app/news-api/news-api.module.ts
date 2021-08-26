c5335e08bd4a2e55976834b2e1d65a3b0830e851
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-08-25 18:22:38 -0700

    paganator added to component  article news

--- src/app/news-api/news-api.module.ts
+++ src/app/news-api/news-api.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NaArticleListComponent } from './na-article-list/na-article-list.component';
 import { TrimOutletNamePipe } from './trim-outlet-name.pipe';
 
-
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   declarations: [NaArticleListComponent, TrimOutletNamePipe],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [NaArticleListComponent]
 })
